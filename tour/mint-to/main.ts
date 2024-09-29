17407a45b930055006ab7af2ce4b07ca93fe2c6d
ErickYangs <1114341853@qq.com>
2024-09-29 10:04:29 +0800

    feat: update text

--- tour/mint-to/main.ts
+++ tour/mint-to/main.ts
@@ -21,7 +21,7 @@ const tokenAccount1Pubkey = new PublicKey("37sAdhEFiYxKnQAm7CPd5GLK1ZxWovqn3p87k
 
 const tokenAccount2Pubkey = new PublicKey("CFEPU5Jd6DNj8gpjPLJ1d9i4xSJDGYNV7n6qw53zE3n1");
 
-// mint token
+// mint token 222
 
 (async () => {
   let tx = new Transaction();
